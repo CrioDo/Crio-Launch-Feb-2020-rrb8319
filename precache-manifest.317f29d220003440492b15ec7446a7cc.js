702b77272be03ada7fb044e976d7c97e4b899f49
CrioDo <rahul@criodo.com>
2020-04-04 21:08:45 +0000

    Updates

---
+++ precache-manifest.317f29d220003440492b15ec7446a7cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6574659cefc9342e8a643b327a3285b",
+    "url": "/Crio-Launch-Feb-2020-rrb8319/index.html"
+  },
+  {
+    "revision": "94ccef9c79276c169ed3",
+    "url": "/Crio-Launch-Feb-2020-rrb8319/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "a5712961cc017e014e32",
+    "url": "/Crio-Launch-Feb-2020-rrb8319/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "94ccef9c79276c169ed3",
+    "url": "/Crio-Launch-Feb-2020-rrb8319/static/js/2.9341dd8f.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Crio-Launch-Feb-2020-rrb8319/static/js/2.9341dd8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5712961cc017e014e32",
+    "url": "/Crio-Launch-Feb-2020-rrb8319/static/js/main.7583ddb8.chunk.js"
+  },
+  {
+    "revision": "8f303ca2841d4c639e78",
+    "url": "/Crio-Launch-Feb-2020-rrb8319/static/js/runtime-main.ae50f755.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-Launch-Feb-2020-rrb8319/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-Launch-Feb-2020-rrb8319/static/media/crio-logo.63be71d0.svg"
+  }
+]);
